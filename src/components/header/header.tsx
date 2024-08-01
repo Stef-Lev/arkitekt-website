ea7b6a5910f99264959e44652bffd9f1732f3e50
Stefanos Leventis <sigianas@yahoo.com>
2024-08-01 22:31:38 +0300

    add header scroll behaviour, fix packages

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,64 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import { cn } from '@/helpers/classnames';
+import { IoMdMenu } from 'react-icons/io';
+
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [headerStyle, setHeaderStyle] = useState('');
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    const handleScroll = () => {
+      if (!isMobile) {
+        const currentScrollTop =
+          window.scrollY || document.documentElement.scrollTop;
+        if (currentScrollTop > lastScrollTop && currentScrollTop > 80) {
+          // Scrolling down on desktop
+          setHeaderStyle('-translate-y-full');
+        } else {
+          // Scrolling up on desktop
+          setHeaderStyle('translate-y-0');
+        }
+        setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop); // For Mobile or negative scrolling
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollTop, isMobile]);
+
   return (
-    <header className="">
-      <h1>Arkitekt</h1>
+    <header
+      className={cn(
+        'bg-teal-700',
+        [
+          isMobile,
+          'fixed top-0 z-50 w-full',
+          'fixed top-0 z-50 w-full transition-transform duration-300',
+        ],
+        [!isMobile, headerStyle],
+      )}
+    >
+      <div
+        className={cn(
+          'flex items-center justify-between',
+          'h-[80px]',
+          'container',
+        )}
+      >
+        <h1>Arkitekt</h1>
+        <IoMdMenu size={40} />
+      </div>
     </header>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,10 +13,6 @@ const config: Config = {
         center: true,
           padding: {
           DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '6rem',
         },
       },
       backgroundImage: {
