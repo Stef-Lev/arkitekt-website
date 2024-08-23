8ba4f25d20ac66ace8ebfbc3e4f6ef7a306d4783
Stefanos Leventis <sigianas@yahoo.com>
2024-08-24 01:07:04 +0300

    add different behaviors to loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,12 +1,15 @@
+import { cn } from '@/helpers/classnames';
+
 interface Props {
   size: number;
+  animation?: 'fade' | 'rotate';
 }
 
-const Loader = ({ size }: Props) => {
+const Loader = ({ size, animation = 'fade' }: Props) => {
   return (
     <div className="absolute inset-0 flex items-center justify-center">
       <div
-        className="loader text-white"
+        className={cn('loader', [animation === 'fade', 'fade', 'rotate'])}
         style={{ width: size, height: size }}
       ></div>
     </div>
