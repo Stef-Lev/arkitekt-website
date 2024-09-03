c1011f3cceaeec7142f035183c9b63ffa73a0273
Stefanos Leventis <sigianas@yahoo.com>
2024-09-04 00:01:31 +0300

    cleanup layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from 'next';
 import Header from '@/components/header/header';
-import Container from '@/components/container/container';
 import Footer from '@/components/footer/footer';
 import { Roboto_Condensed } from 'next/font/google';
 import './globals.css';
-import TitleHeader from '@/components/title-header/title-header';
-import PathFinder from '@/components/path-finder/path-finder';
-import { headers } from 'next/headers';
 import ScrollTopButton from '@/components/scroll-top-button/scroll-top-button';
 
 const robotoCon = Roboto_Condensed({ subsets: ['latin'] });
@@ -21,12 +17,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pagesWithTitleHeader = ['/music', '/videos', '/contact'];
-  const headersList = headers();
-  const path = headersList.get('x-pathname');
-  console.log({ path });
-  // const showTitleHeader = pagesWithTitleHeader.includes(pathname);
-  // console.log(pathname);
   return (
     <html lang="en">
       <body className={robotoCon.className}>
