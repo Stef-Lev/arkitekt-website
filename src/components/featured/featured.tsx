2342aa5d2b28a9ac52b60cef0be9ae2698d02987
Stefanos Leventis <sigianas@yahoo.com>
2024-09-06 00:17:10 +0300

    fix padding and spaces

--- src/components/featured/featured.tsx
+++ src/components/featured/featured.tsx
@@ -5,12 +5,12 @@ import Container from '../container/container';
 const Featured = () => {
   const featuredVideos = content.homePage.featuredVideosIds;
   return (
-    <div className="w-full">
+    <div className="w-full pb-20">
       <Container>
         <h4 className="text-[44px] mb-[32px] text-white text-center">
           Music Videos
         </h4>
-        <div className="flex justify-center flex-col md:flex-row gap-4">
+        <div className="flex justify-center flex-col md:flex-row gap-6">
           {featuredVideos.map((video) => (
             <YoutubeIframe
               key={video}
