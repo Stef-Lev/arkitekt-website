90d6ea807e00b19e148f6f24044da59079fecd2d
Stefanos Leventis <sigianas@yahoo.com>
2026-04-12 00:11:42 +0300

    start removing tailwind

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,28 +1,21 @@
 'use client';
-import { cn } from '@/helpers/classnames';
 import { FaHeart } from 'react-icons/fa';
 import SocialMedia from '@/components/social-media/social-media';
+import styles from './footer.module.css';
 
 const Footer = () => {
   return (
-    <footer
-      className={cn(
-        'p-4 h-[240px] mt-[80px]',
-        'flex justify-center items-center',
-        'text-white bg-black',
-        'relative',
-      )}
-    >
-      <div className={cn('flex flex-col justify-center items-center gap-2')}>
-        <div className="">
-          <div className="flex items-center justify-center text-[20px]">
+    <footer className={styles.footer}>
+      <div className={styles.info}>
+        <div>
+          <div className={styles.credits}>
             Made with{' '}
-            <div className="px-2 text-red-500">
+            <div className={styles.heart}>
               <FaHeart />
             </div>{' '}
             by Arkitekt
           </div>
-          <div className="text-[14px] font-light text-gray-500 py-2">
+          <div className={styles.subtitle}>
             © 2024 Arkitekt. All rights reserved.
           </div>
         </div>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
@@ -7,6 +7,7 @@ import { cn } from '@/helpers/classnames';
 import Drawer from '../drawer/drawer';
 import HamburgerButton from '../hamburger-button/hamburger-button';
 import content from '@/content/content';
+import styles from './header.module.css';
 
 const Header = () => {
   const [lastScrollTop, setLastScrollTop] = useState(0);
@@ -43,46 +44,29 @@ const Header = () => {
     <>
       <header
         className={cn(
-          'bg-[#0a0a0a] text-white',
-          'w-full z-40 fixed top-0 left-0',
-          'transition-transform duration-300',
-          [isVisible, 'translate-y-0', '-translate-y-full'],
+          styles.header,
+          isVisible ? styles.visible : styles.notVisible,
         )}
       >
-        <div
-          className={cn(
-            'container',
-            'flex items-center justify-between',
-            'h-20 mx-auto',
-          )}
-        >
+        <div className={styles.wrapper}>
           <Image
             src="/website-logo.png"
             alt="arkitekt logo"
-            className="aspect-[256/110] hover:cursor-pointer"
+            className={styles.image}
             width={120}
             height={110}
             onClick={() => router.push('/')}
           />
 
           <HamburgerButton
-            className="block md:hidden"
+            className={styles.hamburger}
             isDrawerOpen={isDrawerOpen}
             onClick={toggleDrawer}
           />
 
-          <div className="hidden md:flex">
+          <div className={styles.linksContainer}>
             {content.navbar.items.map((item) => (
-              <Link
-                key={item.href}
-                href={item.href}
-                className={cn(
-                  'py-2 px-4',
-                  'block',
-                  'hover:text-mainBlue',
-                  'hover:underline hover:underline-offset-4',
-                )}
-              >
+              <Link key={item.href} href={item.href} className={styles.link}>
                 {item.value}
               </Link>
             ))}
@@ -90,7 +74,7 @@ const Header = () => {
         </div>
       </header>
       <Drawer
-        className="block md:hidden"
+        className={styles.drawer}
         isDrawerOpen={isDrawerOpen}
         toggleDrawer={toggleDrawer}
         items={content.navbar.items}
